6d919a4a1f606d424bb9150f450a509f01494d13
Alex Spears <aspears@appirio.com>
2019-03-22 11:47:09 -0600

    add python files

---
+++ mathfunction.py
@@ -0,0 +1,12 @@
+'''
+A simple module to show the use of deployment pipelines and error handingling in Bitbucket
+'''
+
+def multiply(a, b):
+    """
+    >>> multiply(4,3)
+    12
+    >>> multiply('a',3)
+    'aaa'
+    """
+    return a * b
